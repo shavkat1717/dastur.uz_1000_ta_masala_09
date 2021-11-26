40dca78e5998bef2d2d9677a8fb55c93a27e57a1
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-26 18:46:14 +0500

    Funksiyaga oid masalalar
    Funksiyaga oid masalalar

---
+++ 01_masala.py
@@ -0,0 +1,12 @@
+a=float(input("1-haqiqiy sonni kiriting: A => "))
+b=float(input("2-haqiqiy sonni kiriting: B => "))
+c=float(input("3-haqiqiy sonni kiriting: C => "))
+d=int(input("4-butun sonni kiriting: D => "))
+e=int(input("5-butun sonni kiriting: E => "))
+def PowerA3(x):
+    return x**3
+print(PowerA3(a))
+print(PowerA3(b))
+print(PowerA3(c)) 
+print(PowerA3(d))
+print(PowerA3(e))
