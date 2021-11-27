81d92ac0807ad2054bb82e74bd2ffdb9352a1a9a
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-27 21:21:37 +0500

    Funksiyaga oid masalalar
    Funksiyaga oid masalalar

---
+++ 02_masala.py
@@ -0,0 +1,10 @@
+a=float(input("Ixtiyoriy haqiqiy sonni kiriting: R => "))
+def PowerA2(x):
+    return x**2
+def PowerA3(x):
+    return x**3
+def PowerA4(x):
+    return x**4
+print(PowerA2(a))
+print(PowerA3(a))
+print(PowerA4(a))
