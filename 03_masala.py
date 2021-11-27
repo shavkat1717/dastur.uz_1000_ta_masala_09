9fa63d1cfef3e3cb751606dbfd54e09995a2f526
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-27 21:23:18 +0500

    Funksiyaga oid masalalar
    Funksiyaga oid masalalar

---
+++ 03_masala.py
@@ -0,0 +1,11 @@
+a=float(input("Ixtiyoriy haqiqiy sonni kiriting: a => "))
+b=float(input("Ixtiyoriy haqiqiy sonni kiriting: b => "))
+c=float(input("Ixtiyoriy haqiqiy sonni kiriting: c => "))
+d=float(input("Ixtiyoriy haqiqiy sonni kiriting: d => "))
+import math
+def MEAN(x,y):
+    print((x+y)/2)
+    print(math.sqrt(x*y))
+MEAN(a, b)  
+MEAN(a, c)
+MEAN(a, d)
