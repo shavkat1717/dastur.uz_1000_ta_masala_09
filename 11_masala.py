780c9076415d48c0a897c9f0dc26ad79836d9312
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-27 23:47:39 +0500

    Funksiyaga oid masalalar
    Funksiyaga oid masalalar

---
+++ 11_masala.py
@@ -0,0 +1,12 @@
+x=int(input("Istalgan 1-sonni kiriting: x => "))
+y=int(input("Istalgan 2-sonni kiriting: y => "))
+c=int(input("Istalgan 3-sonni kiriting: c => "))
+d=int(input("Istalgan 4-sonni kiriting: d => "))
+def Minmax(x):
+    if x>y:
+        print(f"({x}) => (",y,")")
+        print(f"({y}) => (",x,")")
+    else:
+        print(f"({x}) => (",x,")")
+        print(f"({y}) => (",y,")")
+Minmax(x)
