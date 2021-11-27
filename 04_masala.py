a727a80e27b9a1ec404cde4d1452ff293e793a5b
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-27 21:23:46 +0500

    Funksiyaga oid masalalar
    Funksiyaga oid masalalar

---
+++ 04_masala.py
@@ -0,0 +1,9 @@
+a=float(input("1-teng tomonli uchburchakning tomonini kiriting: a => "))
+b=float(input("2-teng tomonli uchburchakning tomonini kiriting: b => "))
+c=float(input("3-teng tomonli uchburchakning tomonini kiriting: c => "))
+def Triangle(x):
+    print((x**2)*(3**1/2)/4)
+    print(3*x)
+Triangle(a)
+Triangle(b)
+Triangle(c)  
