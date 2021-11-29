bbcffc6333e9f9a9104c09cc0f39e18bf121c7e7
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-11-29 21:28:07 +0500

    Funksiyaga oid masalalar
    Funksiyaga oid masalalar

---
+++ 15_masala.py
@@ -0,0 +1,9 @@
+a=float(input("Birinchi sonni kiriting a = "))
+b=float(input("Ikkinchi  sonni kiriting b = "))
+c=float(input("Uchinchi  sonni kiriting c = "))
+def ShiftRight(a,b,c):
+    a==b
+    b==c
+    c==a
+    print(b,c,a)
+ShiftRight(a,b,c)
